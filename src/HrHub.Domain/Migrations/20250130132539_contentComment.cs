1a9d9bd95e5002dad14b78bbdac0324509503fc5
osman.burnak <osman.burnak@asiselektronik.com.tr>
2025-01-30 16:26:55 +0300

    migration

---
+++ src/HrHub.Domain/Migrations/20250130132539_contentComment.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HrHub.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class contentComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StarCount",
+                schema: "public",
+                table: "ContentComments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Positive",
+                schema: "public",
+                table: "CommentVotes",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StarCount",
+                schema: "public",
+                table: "ContentComments");
+
+            migrationBuilder.DropColumn(
+                name: "Positive",
+                schema: "public",
+                table: "CommentVotes");
+        }
+    }
+}
