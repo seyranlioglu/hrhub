545dc800b284980b25b3b5b9f89b2cb523b45c98
Sukru Seyranlioglu <sukru.seyranlioglu@asiselektronik.com.tr>
2025-02-09 23:32:25 +0300

    GetExamList

--- src/HrHub.Domain/Contracts/Dtos/ExamDtos/GetExamListDto.cs
+++ src/HrHub.Domain/Contracts/Dtos/ExamDtos/GetExamListDto.cs
@@ -9,7 +9,6 @@ namespace HrHub.Domain.Contracts.Dtos.ExamDtos
     public class GetExamListDto
     {
         public long? TrainingId { get; set; }
-        public long? ContentId { get; set; }
         public bool? IsActive { get; set; }
     }
 }
